f33aac44ef10005978883df4857b01362eecf423
programatorovic <programatorovic@gmail.com>
2024-12-27 01:43:16 +0100

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,20 +1,39 @@
 //% color=#0fbc11 icon="\uf2db" block="Bits Op"
 namespace bitsOp {
+    /**
+     * Returns the bitwise AND of two numbers
+     * @param a first number
+     * @param b second number
+     */
     //% block
     export function and(a: number, b: number): number {
         return a & b;
     }
 
+    /**
+     * Returns the bitwise OR of two numbers
+     * @param a first number
+     * @param b second number
+     */
     //% block
     export function or(a: number, b: number): number {
         return a | b;
     }
 
+    /**
+     * Returns the bitwise XOR of two numbers
+     * @param a first number
+     * @param b second number
+     */
     //% block
     export function xor(a: number, b: number): number {
         return a ^ b;
     }
 
+    /**
+     * Returns the bitwise NOT of a number
+     * @param a number to invert
+     */
     //% block
     export function inv(a: number): number {
         return ~a;
